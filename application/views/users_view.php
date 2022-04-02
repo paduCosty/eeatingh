a29540b7f067c82f0408c9e8ed0c77cfb5afe5d7
paduCosty <paducosti@yahoo.com>
2022-04-02 17:19:26 +0300

    Add, edit,delete

--- application/views/users_view.php
+++ application/views/users_view.php
@@ -1,51 +1,70 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+      integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 <!DOCTYPE html>
 <html lang="en">
 <head>
 </head>
 <body style="text-align: center;">
+<table class="table table-bordered table-hover">
 
+</table>
 <form method="post"> <!--Create user-->
     <input type="text" name="name"/>
     <input type="text" name="text"/>
     <input type="submit" name="submit" value="Create User"/>
 </form>
-
 <br>
 <!--Show all users-->
-<?php
-if ($users->num_rows() > 0) {
-    $idRow = 1;
-    foreach ($users->result() as $row) {
-        ?>
-        </br></br>
-        <tr>
-            <form method="post">
-                <th><?php echo $row->id ?> |
-                <th name="name"><?php echo $row->name ?> |</th>
-                <th name="text"><?php echo $row->text ?>  </th>
-                <!--delete user-->
-                <div id=<?php echo $idRow ?>></div>
-            </form>
-            <button id="delete<?php echo $idRow ?>" name="button1"
-                    onclick='deleteMenu(
-                    <?php echo $idRow ?>,
-                    <?php echo $users->num_rows() ?>,
-                    <?php echo $row->id ?>)'> X
-            </button>
-
-        </tr>
+<center>
+    <div class='tableBox'>
         <?php
-        $idRow++;
+        if ($users->num_rows() > 0) {
+            $idRow = 1;
+            foreach ($users->result() as $row) {
+                ?>
+
+                <form method="post">
+                    <table class="table table-dark">
+
+                        <tr>
+                            <th><?php echo $row->id ?>
+                            <th name="name"><?php echo $row->name ?></th>
+                            <th name="text"><?php echo $row->text ?></th>
+                            <th style="background-color: green; text-align: center;" id="delete<?php echo $idRow ?>"
+                                name="button1"
+                                onclick='deleteMenu(
+                                <?php echo $idRow ?>,
+                                <?php echo $users->num_rows() ?>,
+                                <?php echo $row->id ?>)'> X
+                            </th>
+                            <th id=<?php echo $idRow ?>></th>
+                            <th name='editUser' type="submint" style="background-color: red">Edit</th>
+
+                            <!--delete user-->
+
+                        </tr>
+
+                    </table>
+                </form>
+                <?php
+                $idRow++;
+            }
+        } else {
+            ?>
+            <h1> Nu exista nici un user.</h1>
+            <?php
+        }
+        ?>
+    </div>
+</center>
+<style type="text/css">
+    .tableBox {
+        width: 600px;
     }
-} else {
-    ?>
-    <h1> Nu exista nici un user.</h1>
-    <?php
-}
-?>
+</style>
 </body>
 </html>
 <script>
@@ -53,13 +72,13 @@ if ($users->num_rows() > 0) {
         let deleteBtn = "delete";
         for (let i = 1; i <= rowsLemgth; ++i) {
             let key = deleteBtn + i;
-            console.log(deleteBtn);
             document.getElementById(i).innerHTML = '';
             document.getElementById(key).style.visibility = 'visible';
         }
         deleteBtn += idRow;
         document.getElementById(deleteBtn).style.visibility = 'hidden';
         let alertBox = document.getElementById(idRow).innerHTML =
-            "<br/><div> <button name='confirmButton' value='" + db_id + "'</button>Yes <button>No</button> </div";
+            "<button style='background-color: red' name='confirmButton' value='" + db_id + "'</button>Yes <button style='background-color: green'>No</button>";
     }
 </script>
+
